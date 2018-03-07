1816b8686d7d712930991900ec0c7404a12e72cc
Mattias Sällström <mattias.sallstrom@omegapoint.se>
2018-03-07 22:09:17 +0100

    Init with Spring start w/o dependencies

---
+++ src/main/java/se/omegapoint/conferre/ConferreApplication.java
@@ -0,0 +1,12 @@
+package se.omegapoint.conferre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferreApplication.class, args);
+	}
+}
---
+++ src/test/java/se/omegapoint/conferre/ConferreApplicationTests.java
@@ -0,0 +1,16 @@
+package se.omegapoint.conferre;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConferreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
